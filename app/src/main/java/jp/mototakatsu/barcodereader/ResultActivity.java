3122862db13fe088ad178fe26f9eb306690555ca
mototakatsu <mototakatsu@gmail.com>
2018-05-06 00:06:55 +0900

    implement function "Copy to clipboard"

--- app/src/main/java/jp/mototakatsu/barcodereader/ResultActivity.java
+++ app/src/main/java/jp/mototakatsu/barcodereader/ResultActivity.java
@@ -1,10 +1,15 @@
 package jp.mototakatsu.barcodereader;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -14,9 +19,23 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        String scanResult = getIntent().getStringExtra(KEY_RESULT);
+        final String scanResult = getIntent().getStringExtra(KEY_RESULT);
         TextView textValue = findViewById(R.id.textValue);
         textValue.setText(scanResult);
+
+        findViewById(R.id.buttonCopy).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipData.Item item = new ClipData.Item(scanResult);
+                String[] mimeType = new String[1];
+                mimeType[0] = ClipDescription.MIMETYPE_TEXT_URILIST;
+                ClipData cd = new ClipData(new ClipDescription("text_data", mimeType), item);
+                ClipboardManager cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                cm.setPrimaryClip(cd);
+                Toast.makeText(ResultActivity.this, R.string.copied_it, Toast.LENGTH_SHORT).show();
+                view.setEnabled(false);
+            }
+        });
     }
 
     public static Intent createIntent(Context context, String scanResult) {
